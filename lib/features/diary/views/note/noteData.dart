5c5196b29dd17c532cdf68f50e786d1ce58ff600
RaphaelGuarim <raphaguarim@gmail.com>
2024-03-28 12:10:21 +0100

    ya

---
+++ lib/features/diary/views/note/noteData.dart
@@ -0,0 +1,15 @@
+class Note {
+  String text;
+  int emotion;
+  List<int> feeling;
+  List<String> image;
+  DateTime date;
+
+  Note({
+    required this.text,
+    required this.emotion,
+    required this.feeling,
+    required this.image,
+    required this.date,
+  });
+}
