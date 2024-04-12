a8009bd6a0d8070e6a0e885776d646d9f0cdb761
Romain B <romain.bourrier@uphf.fr>
2024-04-12 10:21:07 +0200

    add toast

--- lib/features/diary/views/note/widget/firebase_storage_services.dart
+++ lib/features/diary/views/note/widget/firebase_storage_services.dart
@@ -16,21 +16,19 @@ class Storage {
 
   final ManageData manageData = ManageData();
 
-
-  Future<void> uploadFile (
+  Future<void> uploadFile(
     String filePath,
     String fileName,
-      ) async {
-      String UID = await manageData.getUID();
-      File file = File(filePath);
-      String date = DateFormat('ddMMyyyy').format(DateTime.now());
+  ) async {
+    String UID = await manageData.getUID();
+    File file = File(filePath);
+    String date = DateFormat('ddMMyyyy').format(DateTime.now());
 
-
-      try {
-        await storage.ref('$UID/$date/$fileName').putFile(file);
-      } on firebase_core.FirebaseException catch(e){
-        showToast(message: "A problem occured, please try again");
-      }
+    try {
+      await storage.ref('$UID/$date/$fileName').putFile(file);
+    } on firebase_core.FirebaseException catch (e) {
+      showToast(message: "A problem occured, please try again");
+    }
   }
 
   Future<firebase_storage.ListResult> listFiles() async {
@@ -41,83 +39,85 @@ class Storage {
   Future<String> downloadURL(String imageName, DateTime dateTime) async {
     String UID = await manageData.getUID();
     String date = DateFormat('ddMMyyyy').format(dateTime);
-    print(imageName +" - "+date);
-    String downloadURL = await storage.ref('$UID/$date/$imageName').getDownloadURL();
+    print(imageName + " - " + date);
+    String downloadURL =
+        await storage.ref('$UID/$date/$imageName').getDownloadURL();
     return downloadURL;
   }
 
-  Future<String> getFirstName() async{
+  Future<String> getFirstName() async {
     String result = "FirstName";
     try {
       String UID = await manageData.getUID();
-      DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance.collection('informations').doc(UID).get();
-      if(documentSnapshot.exists){
+      DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+          .collection('informations')
+          .doc(UID)
+          .get();
+      if (documentSnapshot.exists) {
         result = documentSnapshot.get('firstName') ?? 'FirstName';
       }
-
-    }catch (e){
+    } catch (e) {
       print("An error occured, please try again $e");
     }
     return result;
   }
 
-Future<String> getLastName() async{
-  String result = "LastName";
-  try {
-    String UID = await manageData.getUID();
-    DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance.collection('informations').doc(UID).get();
-    if(documentSnapshot.exists){
-      result = documentSnapshot.get('lastName') ?? 'LastName';
+  Future<String> getLastName() async {
+    String result = "LastName";
+    try {
+      String UID = await manageData.getUID();
+      DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+          .collection('informations')
+          .doc(UID)
+          .get();
+      if (documentSnapshot.exists) {
+        result = documentSnapshot.get('lastName') ?? 'LastName';
+      }
+    } catch (e) {
+      print("An error occured, please try again $e");
     }
-
-  }catch (e){
-    print("An error occured, please try again $e");
+    return result;
   }
-  return result;
-}
 
   Future<void> insertData(String UID, String firstname, String lastname) async {
-      CollectionReference collRef = FirebaseFirestore.instance.collection('informations');
-      collRef.doc(UID).set({
-        'firstName': firstname,
-        'lastName': lastname,
-      });
+    CollectionReference collRef =
+        FirebaseFirestore.instance.collection('informations');
+    collRef.doc(UID).set({
+      'firstName': firstname,
+      'lastName': lastname,
+    });
   }
 
   Future<void> updateData(String firstname, String lastname) async {
     try {
       String UID = await manageData.getUID();
       print(UID);
-      FirebaseFirestore.instance
-          .collection('informations')
-          .doc(UID)
-          .update({
+      FirebaseFirestore.instance.collection('informations').doc(UID).update({
         'firstName': firstname,
         'lastName': lastname,
       });
-      print("Data updated successfully");
+      showToast(message: 'Your information has been saved');
     } catch (e) {
-      print("An error occurred, please try again $e");
+      showToast(message: 'An error occurred when trying to save your changes');
     }
   }
 
-  Future<void> addNote(Note note)async {
+  Future<void> addNote(Note note) async {
     try {
       Future<String> UID = manageData.getUID();
       String date = DateFormat('MMyyyy').format(note.date);
       UID.then((value) => {
-      FirebaseFirestore.instance.collection(
-      'users/$value/$date').add({
-          'text': note.text,
-          'emotion': note.emotion,
-          'feeling': note.feeling,
-          'image': note.image,
-          'date': note.date.millisecondsSinceEpoch,
-          }).then((value) => {note.id = value as String})
-      });
+            FirebaseFirestore.instance.collection('users/$value/$date').add({
+              'text': note.text,
+              'emotion': note.emotion,
+              'feeling': note.feeling,
+              'image': note.image,
+              'date': note.date.millisecondsSinceEpoch,
+            }).then((value) => {note.id = value as String})
+          });
 
       print("The note has been added");
-    }catch (e){
+    } catch (e) {
       print("An error occured, please try again $e");
     }
   }
@@ -127,17 +127,17 @@ Future<String> getLastName() async{
       Future<String> UID = manageData.getUID();
       String date = DateFormat('MMyyyy').format(note.date);
       UID.then((value) => {
-        FirebaseFirestore.instance
-            .collection('users/$value/$date')
-            .doc(note.id)
-            .delete()
-            .then((value) {
-          print("Note deleted successfully");
-        }).catchError((error) {
-          print("Failed to delete note: $error");
-        })
-      });
-    }catch (e){
+            FirebaseFirestore.instance
+                .collection('users/$value/$date')
+                .doc(note.id)
+                .delete()
+                .then((value) {
+              print("Note deleted successfully");
+            }).catchError((error) {
+              print("Failed to delete note: $error");
+            })
+          });
+    } catch (e) {
       print("An error occured, please try again $e");
     }
   }
@@ -156,11 +156,9 @@ Future<String> getLastName() async{
           'feeling': note.feeling,
           'image': note.image,
           'date': note.date.millisecondsSinceEpoch,
-        })
-            .then((value) {
+        }).then((value) {
           print("Note updated successfully");
-        })
-            .catchError((error) {
+        }).catchError((error) {
           print("Failed to update note: $error");
         });
       });
@@ -169,26 +167,24 @@ Future<String> getLastName() async{
     }
   }
 
-
   Future<List<Note>?> getNotes(DateTime dateTime) async {
     try {
       List<Note> notes = [];
       String UID = await manageData.getUID();
       String date = DateFormat('MMyyyy').format(dateTime);
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('users/$UID/$date').get();
+      QuerySnapshot querySnapshot =
+          await FirebaseFirestore.instance.collection('users/$UID/$date').get();
       for (var doc in querySnapshot.docs) {
-       notes.add(Note(
-          text: doc["text"],
-          image: doc["image"],
-          date: DateTime.fromMillisecondsSinceEpoch(doc["date"]),
-          emotion: doc["emotion"],
-          feeling: doc["feeling"],
-          id: doc.id
-        ));
+        notes.add(Note(
+            text: doc["text"],
+            image: doc["image"],
+            date: DateTime.fromMillisecondsSinceEpoch(doc["date"]),
+            emotion: doc["emotion"],
+            feeling: doc["feeling"],
+            id: doc.id));
       }
       return notes;
-
-    }catch (e){
+    } catch (e) {
       print("An error occured, please try again $e");
     }
     return null;
